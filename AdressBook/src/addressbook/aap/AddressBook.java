696a3c6021970e75bd170f295e1fdf85f74c734e
moe1998 <mohamed.hamza@dawsoncollege.qc.ca>
2017-08-28 10:50:04 -0400

    Ignore Eclipse meta data and bin directories.

---
+++ AdressBook/src/addressbook/aap/AddressBook.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package addressbook.aap;
+import lib.Address;
+
+/**
+ * @author 1633893
+ *
+ */
+public class AddressBook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+
+
+	}
+
+}
